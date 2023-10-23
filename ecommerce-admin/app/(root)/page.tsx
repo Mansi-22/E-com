08eafebfd792c4693ee223ca5759d8481269192d
Mansi-22 <73896751+Mansi-22@users.noreply.github.com>
2023-10-23 15:45:26 +0530

    setup successful
    checked  shadecn/ui  using button componenet

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
-      <p>
-        Hello Admin dashboard
-      </p>
+      <div className="p-4">
+        <Button>Click Me</Button>
+      </div>
     )
 }
