dc89ca6d8f20ee7df25bfb25ed11162c529804fb
Mansi-22 <73896751+Mansi-22@users.noreply.github.com>
2023-10-23 23:49:55 +0530

    Modal Component
    It will always pop up for user to fill the form to select the store.

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,12 +1,26 @@
-import { UserButton } from "@clerk/nextjs";
- 
+"use client";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(()=>{
+      if(!isOpen){
+        onOpen();
+      }
+    }, [ isOpen, onOpen]);
+
     return (
       <div className="p-4">
-        <UserButton afterSignOutUrl="/"/>
+        {/* <Modal title="Test" description="Test desc" isOpen onClose={()=>{}}>
+        Children
+        </Modal>   */}
+        Root page
       </div>
-    )
+    );
 }
 
 export default SetupPage;
